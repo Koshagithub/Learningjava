299ff5870cc55dedf8fa6ca3a94bb778f52cfe61
Koshagithub <116957544+Koshagithub@users.noreply.github.com>
2022-10-30 12:26:04 +0530

    Firstclass file created
    Firstclass file created to check if Java and eclipse are installed properly.

---
+++ Firstclass.java
@@ -0,0 +1,11 @@
+package com.gpr.apps;
+
+public class Firstclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello java");
+
+	}
+
+}
